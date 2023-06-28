7a5258407bfab6973f5eaa1a2511efd152c3b4f9
m1rade <forx98@gmail.com>
2023-06-28 22:04:25 +0300

    Destructure folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import './App.css';
-import { Header } from './layouts/header/Header';
-import { Main } from './layouts/sections/main/Main';
-
+import "./App.css";
+import { Header } from "./layouts/header/Header";
+import { Landing } from "./layouts/landing-page/Landing";
 
 function App() {
     return (
-        <div className="App">
+        <>
             <Header />
-            <Main />
-        </div>
+            <main className="App">
+                <Landing />
+            </main>
+        </>
     );
 }
 
