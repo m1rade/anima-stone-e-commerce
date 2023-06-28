e38e128ed5fd884c047e3590cceaa9f67a11a2c2
m1rade <forx98@gmail.com>
2023-06-28 22:04:49 +0300

    add constant for component

---
+++ src/constants/products.ts
@@ -0,0 +1,13 @@
+export type ProductType = {
+    name: string;
+    category: string;
+    price: string;
+    salesPrice?: string;
+};
+
+export const product: ProductType = {
+    name: "Каминная топка Kratki ZUZIA LPT",
+    category: "Kratki",
+    price: "16 490",
+    salesPrice: "10 990"
+};
