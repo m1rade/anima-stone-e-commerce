ec774b1fe9509cd028eecca008bc2c5a609ce4fc
m1rade <forx98@gmail.com>
2023-07-07 21:26:20 +0300

    Add container to footer

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import logoImg from "../../assets/img/logo-footer.png";
 import { Logo } from "../../components/logo/Logo";
-import { FlexContainer } from "../../components/styled/FlexContainer";
 import { Menu } from "../../components/menu/Menu";
+import { Container } from "../../components/styled/Container";
+import { FlexContainer } from "../../components/styled/FlexContainer";
+import { theme } from "../../styles/theme";
 import { Socials } from "./socials/Socials";
 
 const MenuItems = ["Delivery & returns", "FAQ", "Contacts", "Blog"];
@@ -12,47 +14,48 @@ const socialsItems = ["Facebook", "Instagram", "Twitter", "Youtube", "Pinterest"
 export const Footer = () => {
     return (
         <FooterContainer>
-            <Logo src={logoImg} styles={{ height: "108px" }} />
-            <FlexContainer direction="column" justifyContent="flex-start" gap="4px">
-                <NavHeader>Меню</NavHeader>
-                <Menu items={MenuItems} />
-            </FlexContainer>
-            <FlexContainer direction="column" justifyContent="flex-start" gap="4px">
-                <NavHeader>Каталог</NavHeader>
-                <Menu items={catalogueItems} />
-            </FlexContainer>
-            <FlexContainer direction="column" justifyContent="flex-start" gap="4px">
-                <NavHeader>Контакты для связи</NavHeader>
-                <NavItem>
-                    Телефон: <span>(405) 555-0128</span>
-                </NavItem>
-                <NavItem>
-                    Почта: <span>hello@createx.com</span>
-                </NavItem>
-                <Socials socials={socialsItems} />
-            </FlexContainer>
-            <Address>
-                <small>674 Gonzales Drive. Washington, PA 15301</small>
-            </Address>
+            <GridContainer>
+                <Logo src={logoImg} styles={{ height: "108px" }} />
+                <FlexContainer direction="column" justifyContent="flex-start" gap="4px">
+                    <NavHeader>Меню</NavHeader>
+                    <Menu items={MenuItems} />
+                </FlexContainer>
+                <FlexContainer direction="column" justifyContent="flex-start" gap="4px">
+                    <NavHeader>Каталог</NavHeader>
+                    <Menu items={catalogueItems} />
+                </FlexContainer>
+                <FlexContainer direction="column" justifyContent="flex-start" gap="4px">
+                    <NavHeader>Контакты для связи</NavHeader>
+                    <NavItem>
+                        Телефон: <span>(405) 555-0128</span>
+                    </NavItem>
+                    <NavItem>
+                        Почта: <span>hello@createx.com</span>
+                    </NavItem>
+                    <Socials socials={socialsItems} />
+                </FlexContainer>
+                <Address>
+                    <small>674 Gonzales Drive. Washington, PA 15301</small>
+                </Address>
+            </GridContainer>
         </FooterContainer>
     );
 };
 
 const FooterContainer = styled.footer`
-    max-height: 50vh;
+    background-color: ${theme.colors.secondaryBg};
+    color: ${theme.colors.fontLight};
 
-    background-color: #000;
-    color: #fff;
+    padding-top: 40px;
+`;
 
+const GridContainer = styled(Container)`
     display: grid;
-    grid-template-rows: 5fr 3fr;
     grid-template-columns: 5fr repeat(3, 3fr);
 
     justify-items: center;
     align-items: start;
     grid-row-gap: 30px;
-
-    padding-top: 40px;
 `;
 
 const Address = styled.div`
@@ -61,18 +64,18 @@ const Address = styled.div`
     text-align: center;
 
     padding: 20px 0;
-    
+
     border-top: 1px solid rgba(255, 255, 255, 0.2);
-    
+
     small {
-        color: #fff;
+        color: ${theme.colors.fontLight};
         font-size: 16px;
         font-weight: 400;
     }
 `;
 
 const NavHeader = styled.span`
-    color: #fff;
+    color: ${theme.colors.fontLight};
 
     font-size: 16px;
     font-style: normal;
@@ -84,7 +87,7 @@ const NavHeader = styled.span`
 `;
 
 const NavItem = styled.span`
-    color: #fff;
+    color: ${theme.colors.fontLight};
     font-size: 16px;
     font-weight: 400;
 
